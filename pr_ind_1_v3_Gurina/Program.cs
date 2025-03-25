2c82719a295b1454d35661ae7302cef8bda648b3
GurinaArina0307 <gurinaarina227@gmail.com>
2025-03-25 09:09:08 +0500

    Add files via upload

---
+++ pr_ind_1_v3_Gurina/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+namespace pr_ind_1_v3_Gurina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "file.txt";
+            try
+            {
+                var lines = File.ReadAllLines(file);
+
+                foreach (var line in lines)
+                {
+                    Stack<char> stack = new Stack<char>(line.ToCharArray());
+
+                    Console.WriteLine(new string(stack.ToArray()));
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Произошла ошибка при считывание из файла!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pr_ind_1_v3_Gurina/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("pr_ind_1_v3_Gurina")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pr_ind_1_v3_Gurina")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f4476f29-028d-457b-86b6-0d178185ab1c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
